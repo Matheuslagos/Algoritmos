34a59ec301d087cd435905df08acd09eeed4c789
Matheuslagos <matheuslagos@hotmail.com.br>
2022-07-13 12:32:23 -0300

    first

---
+++ vetores.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "");
+    int *x, valor, y;
+    valor = 35;
+    x = &valor;
+    y = *x;
+    printf("%p\n", &valor);
+    printf("%p\n", x);
+    printf("%p\n", &x);
+    printf("%p\n", *x);
+    printf("%p\n", y);
+}
